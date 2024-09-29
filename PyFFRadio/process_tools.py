372eaf4fb448e639336dadb1b38eab50a62b5523
utak3r <piotr.borys@gmail.com>
2024-09-29 15:19:19 +0200

    Saving the current state of process_tools. See comment.
    So, the current issue is:
    Using TaskGroup reading stdout and stderr does work, but it blocks (this function exits only after it's done)
    Without TaskGroup it doesn't block, but... tasks are never called.

--- PyFFRadio/process_tools.py
+++ PyFFRadio/process_tools.py
@@ -9,37 +9,43 @@ class ProcessRunner:
 
     def __init__(self) -> None:
         self.process = None
+        self.read_stdout_task = None
+        self.read_stderr_task = None
     
     def __del__(self):
-        self.terminate()
+        if self.process is not None:
+            self.process.terminate()
     
     def run_command(self, program, *args):
         self.add_log(f'{program} {args}')
         asyncio.run(self.run(program, *args))
     
-    async def read_stream(self, stream):
-        while self.process.returncode is None:
+    async def read_stream(self, process, stream):
+        while process.returncode is None:
             try:
                 line = await stream.readline()
+                if line:
+                    line = line.decode("utf-8")[:-1]
+                    print(line)
+                    #self.add_log(line)
             except (asyncio.LimitOverrunError, ValueError):
                 continue
-            if line:
-                line = line.decode("utf-8")[:-1]
-                print(line)
-                self.add_log(line)
-            else:
-                break
 
     # WARNING!
     # This code REQUIRES Python 3.11 at least!
     async def run(self, program, *args):
         try:
             self.process = await asyncio.create_subprocess_exec(program, *args, stdin=asyncio.subprocess.DEVNULL, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-            async with asyncio.TaskGroup() as tg:
-                read_stdout_task = asyncio.create_task(self.read_stream(self.process.stdout))
-                read_stderr_task = asyncio.create_task(self.read_stream(self.process.stderr))
         finally:
-            self.add_log("Running command has finished: " + f'{program} {args}')
+            self.add_log("Run command: " + f'{program} {args}')
+        # So, the current issue is:
+        # Using TaskGroup reading stdout and stderr does work, but it blocks (this function exits only after it's done)
+        # Without TaskGroup it doesn't block, but... tasks are never called.
+        #async with asyncio.TaskGroup() as tg:
+        #    self.read_stdout_task = tg.create_task(self.read_stream(self.process, self.process.stdout))
+        #    self.read_stderr_task = tg.create_task(self.read_stream(self.process, self.process.stderr))
+        self.read_stdout_task = asyncio.create_task(self.read_stream(self.process, self.process.stdout))
+        self.read_stderr_task = asyncio.create_task(self.read_stream(self.process, self.process.stderr))
 
 
     def terminate(self):
